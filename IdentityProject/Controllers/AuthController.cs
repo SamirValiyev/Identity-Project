c6f8de67278cb5fc015f16342fc49f347ca62319
SamirValiyev <vliyevsamir5@gmail.com>
2025-05-26 14:47:54 +0400

    update project

--- IdentityProject/Controllers/AuthController.cs
+++ IdentityProject/Controllers/AuthController.cs
@@ -1,9 +1,11 @@
 ﻿using IdentityProject.Enums;
 using IdentityProject.Models;
 using IdentityProject.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics.Eventing.Reader;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace IdentityProject.Controllers
@@ -12,11 +14,13 @@ namespace IdentityProject.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<AppRole> _roleManager;
 
-        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         [HttpGet]   
@@ -44,10 +48,21 @@ namespace IdentityProject.Controllers
                     Email = model.Email,
                     UserName = model.Username,
                     Gender = model.Gender,
+                    EmailConfirmed=true
 
                 };
                 var identityResult= await _userManager.CreateAsync(appUser,model.Password);
-                if (identityResult.Succeeded) return RedirectToAction("Index","Home");
+                if (identityResult.Succeeded)
+                {
+                    
+                    //await _roleManager.CreateAsync(new AppRole
+                    //{
+                    //    Name = "Admin",
+                    //    CreatedDate = DateTime.Now
+                    //});
+                    await _userManager.AddToRoleAsync(appUser, "Admin");
+                    return RedirectToAction("Index", "Home");
+                }
                 else
                 {
                     foreach(var error in identityResult.Errors)
@@ -74,6 +89,7 @@ namespace IdentityProject.Controllers
                 var signInResult=await _signInManager.PasswordSignInAsync(model.UserName, model.Password,false,true);
                 if (signInResult.Succeeded)
                 {
+                    return RedirectToAction("Index","Home");
                     // signin ugurlu
                 }
                 else if (signInResult.IsLockedOut)
@@ -82,10 +98,19 @@ namespace IdentityProject.Controllers
                 }
                 else if (signInResult.IsNotAllowed)
                 {
-                    // email ve ya nomre dogrulamasi(tesdiqlenmesi)
+                    // email ve ya nomre dogrulamasi(tesdiqlenmemesi)
                 }
             }
             return View(model);
+        }
+
+        [Authorize(Roles ="Admin")]
+        public IActionResult GetUserDetail()
+        {
+            var user = User.Identity.Name;
+            var role=User.Claims.FirstOrDefault(x=>x.Type==ClaimTypes.Role);
+
+            return View();
         }
     }
 }
